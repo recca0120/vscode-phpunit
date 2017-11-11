07d30403689d65cf22cbea4d7bad04068b1f690d
recca0120 <recca0120@gmail.com>
2017-11-12 03:04:04 +0800

    import

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,6 +1,6 @@
-import _minimist = require('minimist');
-const _minimistString = require('minimist-string');
-const x2jsCore = require('x2js');
+import * as X2JS from 'x2js';
+import * as _minimist from 'minimist';
+import * as _minimistString from 'minimist-string';
 
 export function isWindows(): boolean {
     return /win32|mswin(?!ce)|mingw|bccwin|cygwin/i.test(process.platform);
@@ -20,7 +20,7 @@ export function minimistString(...x) {
     return _minimistString(...x);
 }
 
-const x2js = new x2jsCore();
+const x2js = new X2JS();
 export function xml2js(content): Promise<any> {
     return new Promise(resolve => {
         resolve(x2js.xml2js(content));
