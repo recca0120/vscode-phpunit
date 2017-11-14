1857a7cf66d6c5d9439fb3bb5d2364a02487b97a
recca0120 <recca0120@gmail.com>
2017-11-14 12:48:15 +0800

    update

--- src/extension.ts
+++ src/extension.ts
@@ -20,20 +20,14 @@ export function activate(context: ExtensionContext) {
         .set('extensionPath', context.extensionPath);
 
     const outputChannel: OutputChannel = window.createOutputChannel(container.name);
-
     const diagnostics: DiagnosticCollection = languages.createDiagnosticCollection(container.name);
-
     const decorateManager = new DecorateManager(container);
-
     const diagnosticManager = new DiagnosticManager(diagnostics);
-
     const phpunit: PHPUnit = new PHPUnit(container.parserFactory, container.processFactory, container.files);
-
-    phpunit
-        .on('before', () => outputChannel.clear())
-        .on('stdout', (buffer: Buffer) => outputChannel.append(buffer.toString()));
-
     const testRunner = new TestRunner(container, phpunit, decorateManager, diagnosticManager);
+
+    phpunit.on('before', () => outputChannel.clear());
+    phpunit.on('stdout', (buffer: Buffer) => outputChannel.append(buffer.toString()));
 
     context.subscriptions.push(testRunner.subscribe(commands));
 
