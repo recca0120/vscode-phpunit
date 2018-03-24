f81f16f560734f685a9342938f9e359db4f2d7cf
recca0120 <recca0120@gmail.com>
2018-03-24 18:19:42 +0800

    refactoring filesystem

--- server/src/Filesystem.ts
+++ server/src/Filesystem.ts
@@ -1,12 +1,33 @@
-import { normalize } from 'path';
 import { readFileSync } from 'fs';
+import { isWindows } from './helpers';
+
+export class POSIX {
+    protected separator: string = '/';
+
+    normalizePath(path: string) {
+        return path.replace(/^file:\/\//, '');
+    }
+}
+
+export class WINDOWS {
+    protected separator: string = '\\';
+
+    normalizePath(path: string) {
+        return path
+            .replace(/^file:\/\//, '')
+            .replace(/^\/(\w)%3A/, '$1:')
+            .replace(/\//g, this.separator);
+    }
+}
 
 export class Filesystem {
+    constructor(private instance = isWindows() ? new WINDOWS() : new POSIX()) {}
+
     get(path: string): string {
         return readFileSync(this.normalizePath(path)).toString('utf8');
     }
 
     normalizePath(path: string) {
-        return normalize(path.replace(/^file:\/\//, '').replace(/^\/(\w)%3A/, '$1:'));
+        return this.instance.normalizePath(path);
     }
 }
--- server/tests/Filesystem.test.ts
+++ server/tests/Filesystem.test.ts
@@ -1,7 +1,6 @@
-import { Filesystem } from './../src/Filesystem';
+import { Filesystem, POSIX, WINDOWS } from './../src/Filesystem';
 import { readFileSync } from 'fs';
 import { resolve } from 'path';
-import {isWindows} from './../src/helpers';
 
 describe('Filesystem Test', () => {
     it('it should get content from file', () => {
@@ -11,11 +10,18 @@ describe('Filesystem Test', () => {
         expect(files.get(path)).toEqual(readFileSync(path).toString('utf8'));
     });
 
-    it('normalize path', () => {
-        const files: Filesystem = new Filesystem();
-        const path = isWindows() === true ? 'file:///c%3A/foo/bar' : 'file:///usr/bin';
-        const result = isWindows() === true ? 'c:\\foo\\bar' : '/usr/bin';
+    it('it should normalize path when os is posix', () => {
+        const files: POSIX = new POSIX();
+        expect(files.normalizePath('file:///foo/bar')).toEqual('/foo/bar');
+    });
 
-        expect(files.normalizePath(path)).toEqual(result);
+    it('it should normalize path when os is windows', () => {
+        const files: WINDOWS = new WINDOWS();
+        expect(files.normalizePath('file:///c%3A/foo/bar')).toEqual('c:\\foo\\bar');
+    });
+
+    it('it should normalize path', () => {
+        const files: Filesystem = new Filesystem(new WINDOWS());
+        expect(files.normalizePath('file:///c%3A/foo/bar')).toEqual('c:\\foo\\bar');
     });
 });
