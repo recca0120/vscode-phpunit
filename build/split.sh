aa3f3f50d2f3b982a7abd6553f71dafa3cdbc761
recca0120 <recca0120@gmail.com>
2018-03-31 00:00:23 +0800

    split

---
+++ build/split.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+split()
+{
+    SUBDIR=$1
+    SPLIT=$2
+    HEADS=$3
+
+    mkdir -p $SUBDIR;
+
+    pushd $SUBDIR;
+
+    for HEAD in $HEADS
+    do
+
+        mkdir -p $HEAD
+
+        pushd $HEAD
+
+        git subsplit init git@github.com:recca0120/vscode-phpunit.git
+        git subsplit update
+
+        time git subsplit publish --heads="$HEAD" --no-tags "$SPLIT"
+
+        popd
+
+    done
+
+    popd
+}
+
+split server             server:git@github.com:recca0120/phpunit-lsp.git                       "master"
