c7d604f4b47ed84d0b3496835e3d119969ed0940
recca0120 <recca0120@gmail.com>
2018-04-15 22:58:24 +0800

    diagnostic collection name

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -42,6 +42,7 @@ export function activate(context: ExtensionContext) {
         },
         outputChannel: window.createOutputChannel('PHPUnit'),
         revealOutputChannelOn: RevealOutputChannelOn.Error,
+        diagnosticCollectionName: 'PHPUnit',
     };
 
     const client = new LanguageClient('phpunit', 'PHPUnit Language Server', serverOptions, clientOptions);
