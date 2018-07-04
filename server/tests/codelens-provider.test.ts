6fdcb56961b0c4841aa9e1f9ac463b26e33dc9eb
recca0120 <recca0120@gamil.com>
2018-07-04 11:27:36 +0800

    codelens provider test

---
+++ server/tests/codelens-provider.test.ts
@@ -0,0 +1,40 @@
+import { CodeLensProvider } from '../src/codelens-provider';
+import { TestSuite } from '../src/phpunit/test-suite';
+describe('CodeLensProvider Test', () => {
+    it('it should return codelens', async () => {
+        const testSuite: TestSuite = new TestSuite();
+        const codelensProvider: CodeLensProvider = new CodeLensProvider(testSuite);
+
+        spyOn(testSuite, 'parse').and.returnValue([
+            {
+                kind: 'class',
+                name: 'AssertionsTest',
+                range: { end: { character: 1, line: 10 }, start: { character: 0, line: 4 } },
+                uri: 'PHPUnitTest.php',
+            },
+            {
+                kind: 'method',
+                name: 'test_method',
+                range: { end: { character: 5, line: 9 }, start: { character: 11, line: 6 } },
+                uri: 'PHPUnitTest.php',
+            },
+        ]);
+
+        expect(codelensProvider.formText('text', 'PHPUnitTest.php')).toEqual([
+            {
+                command: { arguments: ['PHPUnitTest.php'], command: 'phpunit.test', title: 'Run Test' },
+                data: { textDocument: { uri: 'PHPUnitTest.php' } },
+                range: { end: { character: 1, line: 10 }, start: { character: 0, line: 4 } },
+            },
+            {
+                command: {
+                    arguments: ['PHPUnitTest.php', '--filter', '^.*::test_method( with data set .*)?$'],
+                    command: 'phpunit.test.file',
+                    title: 'Run Test',
+                },
+                data: { textDocument: { uri: 'PHPUnitTest.php' } },
+                range: { end: { character: 5, line: 9 }, start: { character: 11, line: 6 } },
+            },
+        ]);
+    });
+});
