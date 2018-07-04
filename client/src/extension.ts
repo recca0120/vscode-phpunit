de65e2c8248df9d4a826fcafddf1e1f726e7beb7
recca0120 <recca0120@gamil.com>
2018-07-04 12:26:11 +0800

    rename

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -36,7 +36,7 @@ export function activate(context: ExtensionContext) {
     };
 
     // Create the language client and start the client.
-    client = new LanguageClient('languageServerExample', 'Language Server Example', serverOptions, clientOptions);
+    client = new LanguageClient('phpunitLanguageServer', 'PHPUnit Language Server', serverOptions, clientOptions);
 
     // Start the client. This will also launch the server
     client.start();
