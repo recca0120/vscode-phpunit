a9224248957064359c05104a6a19594d75ce3962
recca0120 <recca0120@gamil.com>
2018-07-04 12:23:23 +0800

    refactoring

--- server/src/diagnostic-provider.ts
+++ server/src/diagnostic-provider.ts
@@ -1,7 +1,7 @@
 import { Filesystem, Factory as FilesystemFactory } from './filesystem';
 import { Text } from './support/text';
 import { Test, Type, Detail, Fault } from './phpunit/common';
-import { Diagnostic, DiagnosticSeverity, DiagnosticRelatedInformation } from 'vscode-languageserver-protocol';
+import { Diagnostic, DiagnosticSeverity, DiagnosticRelatedInformation, Range } from 'vscode-languageserver-protocol';
 
 export class DiagnosticProvider {
     constructor(private text: Text = new Text(), private files: Filesystem = new FilesystemFactory().create()) {}
@@ -9,11 +9,15 @@ export class DiagnosticProvider {
     async asDiagnosticGroup(tests: Test[]): Promise<Map<string, Diagnostic[]>> {
         tests = await Promise.all(
             tests.filter((test: Test) => test.type !== Type.PASSED).map(async (test: Test) => {
-                test.range = await this.text.line(test.file, test.line - 1);
+                test.range = await this.findRange(test);
+
+                if (!test.fault || !test.fault.details) {
+                    return test;
+                }
 
                 test.fault.details = await Promise.all(
                     test.fault.details.map(async (detail: Detail) => {
-                        detail.range = await this.text.line(detail.file, detail.line - 1);
+                        detail.range = await this.findRange(detail);
 
                         return detail;
                     })
@@ -23,6 +27,10 @@ export class DiagnosticProvider {
             })
         );
 
+        return this.groupBy(tests);
+    }
+
+    private groupBy(tests: Test[]): Map<string, Diagnostic[]> {
         return tests.reduce((diagnosticGroup: Map<string, Diagnostic[]>, test: Test): Map<string, Diagnostic[]> => {
             const uri = this.files.uri(test.file);
             const diagnostics: Diagnostic[] = diagnosticGroup.get(uri) || [];
@@ -34,19 +42,27 @@ export class DiagnosticProvider {
                 severity: test.type === Type.RISKY ? DiagnosticSeverity.Warning : DiagnosticSeverity.Error,
                 range: test.range,
                 message: message,
-                relatedInformation: details.map((detail: Detail) => {
-                    return DiagnosticRelatedInformation.create(
-                        {
-                            range: detail.range,
-                            uri: this.files.uri(detail.file),
-                        },
-                        message
-                    );
-                }),
+                relatedInformation: this.asDiagnosticRelationInformations(details, message),
                 source: 'PHPUnit',
             });
 
             return diagnosticGroup.set(uri, diagnostics);
         }, new Map<string, Diagnostic[]>());
+    }
+
+    private asDiagnosticRelationInformations(details: Detail[], message: string): DiagnosticRelatedInformation[] {
+        return details.map((detail: Detail) => {
+            return DiagnosticRelatedInformation.create(
+                {
+                    range: detail.range,
+                    uri: this.files.uri(detail.file),
+                },
+                message
+            );
+        });
+    }
+
+    private async findRange(obj: Test | Detail): Promise<Range> {
+        return await this.text.line(obj.file, obj.line - 1);
     }
 }
