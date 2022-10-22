9aa2cb243eacdf2f641e7279189003c308fc38a3
recca0120 <recca0120@gmail.com>
2022-10-22 12:41:37 +0800

    test extension

--- src/extension.ts
+++ src/extension.ts
@@ -23,4 +23,4 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,20 @@
-import * as assert from 'assert';
+import * as chai from 'chai';
+import * as sinon from 'sinon';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	test('Sample test', async () => {
+		const showInformationMessage = sinon.spy(vscode.window, 'showInformationMessage');
+		const ext = vscode.extensions.getExtension("recca0120.vscode-phpunit")!;
+		await ext.activate();
+
+		vscode.commands.executeCommand("vscode-phpunit.helloWorld");
+
+		chai.expect(showInformationMessage.calledWith('Hello World from vscode-phpunit!')).to.be.true;
 	});
 });
