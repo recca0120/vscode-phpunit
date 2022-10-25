a4ed08c5ac0b7bab9363822d2e62e88103ad0208
recca0120 <recca0120@gmail.com>
2022-10-25 13:23:24 +0800

    unescape value

--- src/phpunit/problem-matcher.ts
+++ src/phpunit/problem-matcher.ts
@@ -1,6 +1,52 @@
 import * as parser from 'yargs-parser';
 import { Arguments } from 'yargs-parser';
 
+class EscapeValue {
+    private values = {
+        escape: ['||', "|'", '|n', '|r', '|]', '|['],
+        unescape: ['|', "'", '\n', '\r', ']', '['],
+    };
+
+    private patterns: { unescape: RegExp[]; escape: RegExp[] };
+
+    constructor() {
+        this.patterns = {
+            escape: this.toRegExp(this.values.escape),
+            unescape: this.toRegExp(this.values.unescape),
+        };
+    }
+
+    public escape(value: string | number) {
+        return this.doEscape(value, this.patterns.unescape, this.values.escape);
+    }
+
+    public unescape(value: string | number) {
+        return this.doEscape(value, this.patterns.escape, this.values.unescape);
+    }
+
+    private doEscape(value: string | number, from: RegExp[], to: string[]) {
+        if (typeof value === 'number') {
+            return value;
+        }
+
+        for (const x in from) {
+            value = value.replace(from[x], to[x]);
+        }
+
+        return value;
+    }
+
+    private toRegExp(values: string[]) {
+        return values.map((str) => {
+            str = str.replace(/([|\]\[])/g, (m) => `\\${m}`);
+
+            return new RegExp(str, 'g');
+        });
+    }
+}
+
+export const escapeValue = new EscapeValue();
+
 class TeamcityParser {
     private readonly pattern = /^\s*#+teamcity/;
 
@@ -11,6 +57,9 @@ class TeamcityParser {
             .replace(/^\[|\]$/g, '');
 
         const { _, $0, ...argv } = this.toTeamcityArgv(text);
+        for (const x in argv) {
+            argv[x] = escapeValue.unescape(argv[x]);
+        }
 
         return { ...argv };
     }
--- tests/phpunit/problem-matcher.test.ts
+++ tests/phpunit/problem-matcher.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from '@jest/globals';
-import { teamcityParser as parser } from '../../src/phpunit/problem-matcher';
+import { teamcityParser as parser, escapeValue } from '../../src/phpunit/problem-matcher';
 
 describe('Problem Matcher Test', () => {
     describe('Teamcity Parser', () => {
@@ -66,8 +66,9 @@ describe('Problem Matcher Test', () => {
                 teamcity: 'testFailed',
                 name: 'test_failed',
                 message: 'Failed asserting that false is true.',
-                details:
-                    ' C:\\Users\\recca\\Desktop\\vscode-phpunit\\__tests__\\fixtures\\project-stub\\tests\\AssertionsTest.php:22|n ',
+                details: escapeValue.unescape(
+                    ' C:\\Users\\recca\\Desktop\\vscode-phpunit\\__tests__\\fixtures\\project-stub\\tests\\AssertionsTest.php:22|n '
+                ),
                 duration: 0,
                 flowId: 8024,
             });
@@ -81,12 +82,17 @@ describe('Problem Matcher Test', () => {
                 teamcity: 'testFailed',
                 name: 'test_is_not_same',
                 message: 'Failed asserting that two arrays are identical.',
-                details:
-                    ' C:\\Users\\recca\\Desktop\\vscode-phpunit\\__tests__\\fixtures\\project-stub\\tests\\AssertionsTest.php:27|n ',
+                details: escapeValue.unescape(
+                    ' C:\\Users\\recca\\Desktop\\vscode-phpunit\\__tests__\\fixtures\\project-stub\\tests\\AssertionsTest.php:27|n '
+                ),
                 duration: 0,
                 type: 'comparisonFailure',
-                actual: "Array &0 (|n    |'e|' => |'f|'|n    0 => |'g|'|n    1 => |'h|'|n)",
-                expected: "Array &0 (|n    |'a|' => |'b|'|n    |'c|' => |'d|'|n)",
+                actual: escapeValue.unescape(
+                    "Array &0 (|n    |'e|' => |'f|'|n    0 => |'g|'|n    1 => |'h|'|n)"
+                ),
+                expected: escapeValue.unescape(
+                    "Array &0 (|n    |'a|' => |'b|'|n    |'c|' => |'d|'|n)"
+                ),
                 flowId: 8024,
             });
         });
@@ -98,10 +104,12 @@ describe('Problem Matcher Test', () => {
             expect(parser.parse(teamcity)).toEqual({
                 teamcity: 'testFailed',
                 name: 'test_sum_item_method_not_call',
-                message:
-                    'Mockery\\Exception\\InvalidCountException : Method test(<Any Arguments>) from Mockery_0_Recca0120_VSCode_Item_Recca0120_VSCode_Item should be called|r|n exactly 1 times but called 0 times.',
-                details:
-                    ' C:\\Users\\recca\\Desktop\\vscode-phpunit\\__tests__\\fixtures\\project-stub\\vendor\\mockery\\mockery\\library\\Mockery\\CountValidator\\Exact.php:38|n C:\\Users\\recca\\Desktop\\vscode-phpunit\\__tests__\\fixtures\\project-stub\\vendor\\mockery\\mockery\\library\\Mockery\\Expectation.php:308|n C:\\Users\\recca\\Desktop\\vscode-phpunit\\__tests__\\fixtures\\project-stub\\vendor\\mockery\\mockery\\library\\Mockery\\ExpectationDirector.php:119|n C:\\Users\\recca\\Desktop\\vscode-phpunit\\__tests__\\fixtures\\project-stub\\vendor\\mockery\\mockery\\library\\Mockery\\Container.php:299|n C:\\Users\\recca\\Desktop\\vscode-phpunit\\__tests__\\fixtures\\project-stub\\vendor\\mockery\\mockery\\library\\Mockery\\Container.php:284|n C:\\Users\\recca\\Desktop\\vscode-phpunit\\__tests__\\fixtures\\project-stub\\vendor\\mockery\\mockery\\library\\Mockery.php:204|n C:\\Users\\recca\\Desktop\\vscode-phpunit\\__tests__\\fixtures\\project-stub\\vendor\\mockery\\mockery\\library\\Mockery\\Adapter\\Phpunit\\MockeryPHPUnitIntegration.php:68|n C:\\Users\\recca\\Desktop\\vscode-phpunit\\__tests__\\fixtures\\project-stub\\vendor\\mockery\\mockery\\library\\Mockery\\Adapter\\Phpunit\\MockeryPHPUnitIntegration.php:43|n C:\\Users\\recca\\Desktop\\vscode-phpunit\\__tests__\\fixtures\\project-stub\\vendor\\mockery\\mockery\\library\\Mockery\\Adapter\\Phpunit\\MockeryPHPUnitIntegrationAssertPostConditions.php:29|n ',
+                message: escapeValue.unescape(
+                    'Mockery\\Exception\\InvalidCountException : Method test(<Any Arguments>) from Mockery_0_Recca0120_VSCode_Item_Recca0120_VSCode_Item should be called|r|n exactly 1 times but called 0 times.'
+                ),
+                details: escapeValue.unescape(
+                    ' C:\\Users\\recca\\Desktop\\vscode-phpunit\\__tests__\\fixtures\\project-stub\\vendor\\mockery\\mockery\\library\\Mockery\\CountValidator\\Exact.php:38|n C:\\Users\\recca\\Desktop\\vscode-phpunit\\__tests__\\fixtures\\project-stub\\vendor\\mockery\\mockery\\library\\Mockery\\Expectation.php:308|n C:\\Users\\recca\\Desktop\\vscode-phpunit\\__tests__\\fixtures\\project-stub\\vendor\\mockery\\mockery\\library\\Mockery\\ExpectationDirector.php:119|n C:\\Users\\recca\\Desktop\\vscode-phpunit\\__tests__\\fixtures\\project-stub\\vendor\\mockery\\mockery\\library\\Mockery\\Container.php:299|n C:\\Users\\recca\\Desktop\\vscode-phpunit\\__tests__\\fixtures\\project-stub\\vendor\\mockery\\mockery\\library\\Mockery\\Container.php:284|n C:\\Users\\recca\\Desktop\\vscode-phpunit\\__tests__\\fixtures\\project-stub\\vendor\\mockery\\mockery\\library\\Mockery.php:204|n C:\\Users\\recca\\Desktop\\vscode-phpunit\\__tests__\\fixtures\\project-stub\\vendor\\mockery\\mockery\\library\\Mockery\\Adapter\\Phpunit\\MockeryPHPUnitIntegration.php:68|n C:\\Users\\recca\\Desktop\\vscode-phpunit\\__tests__\\fixtures\\project-stub\\vendor\\mockery\\mockery\\library\\Mockery\\Adapter\\Phpunit\\MockeryPHPUnitIntegration.php:43|n C:\\Users\\recca\\Desktop\\vscode-phpunit\\__tests__\\fixtures\\project-stub\\vendor\\mockery\\mockery\\library\\Mockery\\Adapter\\Phpunit\\MockeryPHPUnitIntegrationAssertPostConditions.php:29|n '
+                ),
                 duration: 13,
                 flowId: 8024,
             });
@@ -115,8 +123,9 @@ describe('Problem Matcher Test', () => {
                 teamcity: 'testIgnored',
                 name: 'test_skipped',
                 message: 'The MySQLi extension is not available.',
-                details:
-                    ' C:\\Users\\recca\\Desktop\\vscode-phpunit\\__tests__\\fixtures\\project-stub\\tests\\AssertionsTest.php:45|n ',
+                details: escapeValue.unescape(
+                    ' C:\\Users\\recca\\Desktop\\vscode-phpunit\\__tests__\\fixtures\\project-stub\\tests\\AssertionsTest.php:45|n '
+                ),
                 duration: 0,
                 flowId: 8024,
             });
@@ -130,8 +139,9 @@ describe('Problem Matcher Test', () => {
                 teamcity: 'testIgnored',
                 name: 'test_incomplete',
                 message: 'This test has not been implemented yet.',
-                details:
-                    ' C:\\Users\\recca\\Desktop\\vscode-phpunit\\__tests__\\fixtures\\project-stub\\tests\\AssertionsTest.php:50|n ',
+                details: escapeValue.unescape(
+                    ' C:\\Users\\recca\\Desktop\\vscode-phpunit\\__tests__\\fixtures\\project-stub\\tests\\AssertionsTest.php:50|n '
+                ),
                 duration: 0,
                 flowId: 8024,
             });
