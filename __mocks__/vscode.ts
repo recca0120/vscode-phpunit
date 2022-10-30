0cd53c2326ad6247b726995e061cf1e9103ea2ed
recca0120 <recca0120@gmail.com>
2022-10-31 06:57:47 +0800

    mock vscode

---
+++ __mocks__/vscode.ts
@@ -0,0 +1,199 @@
+import { readFileSync } from 'fs';
+import {
+    CancellationToken,
+    MarkdownString,
+    TestController,
+    TestItem,
+    TestItemCollection,
+    TestRunRequest,
+    TestTag,
+    Uri,
+    WorkspaceFolder,
+} from 'vscode';
+import { glob } from 'glob';
+import { URI } from 'vscode-uri';
+
+enum TestRunProfileKind {
+    Run = 1,
+    Debug = 2,
+    Coverage = 3,
+}
+
+class FakeTestItemCollection implements Iterable<[id: string, testItem: TestItem]> {
+    private items = new Map<string, TestItem>();
+
+    get size() {
+        return this.items.size;
+    }
+
+    add(item: TestItem) {
+        this.items.set(item.id, item);
+    }
+
+    get(itemId: string) {
+        return this.items.get(itemId);
+    }
+
+    replace(items: readonly TestItem[]) {
+        this.items.clear();
+        items.forEach((item) => this.add(item));
+    }
+
+    forEach(
+        callback: (item: TestItem, collection: TestItemCollection) => unknown,
+        _thisArg?: any
+    ): void {
+        for (const [, item] of this.items) {
+            callback(item, Object.assign(this) as TestItemCollection);
+        }
+    }
+
+    [Symbol.iterator](): Iterator<[id: string, testItem: TestItem]> {
+        return this.items.entries();
+    }
+}
+
+const createTestItem = jest
+    .fn()
+    .mockImplementation((id: string, label: string, uri?: Uri): TestItem => {
+        return {
+            id,
+            uri,
+            children: new FakeTestItemCollection(),
+            label,
+        } as any;
+    });
+
+const createRunProfile = jest
+    .fn()
+    .mockImplementation(
+        (
+            label: string,
+            kind: TestRunProfileKind,
+            runHandler: (
+                request: TestRunRequest,
+                token: CancellationToken
+            ) => Thenable<void> | void,
+            isDefault?: boolean,
+            tag?: TestTag
+        ) => ({ label, kind, isDefault, tag, runHandler })
+    );
+
+const createTestRun = jest
+    .fn()
+    .mockImplementation((_request: TestRunRequest, name?: string, persist?: boolean) => {
+        return {
+            name: name,
+            // token: CancellationToken;
+            isPersisted: !!persist,
+            enqueued: jest.fn(),
+            started: jest.fn(),
+            skipped: jest.fn(),
+            failed: jest.fn(),
+            errored: jest.fn(),
+            passed: jest.fn(),
+            appendOutput: jest.fn(),
+            end: jest.fn(),
+        };
+    });
+
+const createTestController = jest
+    .fn()
+    .mockImplementation((id: string, label: string): TestController => {
+        return {
+            id,
+            label,
+            createRunProfile,
+            createTestItem,
+            createTestRun,
+            items: new FakeTestItemCollection(),
+        } as any;
+    });
+
+const tests = { createTestController };
+
+const TestMessage = {
+    diff: jest
+        .fn()
+        .mockImplementation(
+            (message: string | MarkdownString, expected: string, actual: string) => {
+                return { message, expected, actual };
+            }
+        ),
+};
+
+class Disposable {
+    dispose = (): any => jest.fn();
+}
+
+const Location = jest.fn().mockImplementation((uri: Uri, rangeOrPosition: any) => {
+    return { uri, range: rangeOrPosition };
+});
+
+const Range = jest.fn().mockImplementation((start: any, end: any) => {
+    return { start, end };
+});
+
+const Position = jest.fn().mockImplementation((line: number, character: number) => {
+    return { line, character };
+});
+
+const CancellationTokenSource = jest.fn().mockImplementation(() => {
+    return {
+        token: { isCancellationRequested: false, onCancellationRequested: jest.fn() },
+        cancel: jest.fn(),
+        dispose: new Disposable(),
+    };
+});
+
+const workspace = {
+    workspaceFolders: [],
+    textDocuments: [],
+    findFiles: jest.fn().mockImplementation((pattern) => {
+        return glob
+            .sync(pattern.pattern, {
+                absolute: true,
+                ignore: ['**/node_modules/**', '**/.git/**'],
+                cwd: pattern.uri.fsPath,
+            })
+            .map((file) => URI.parse(file));
+    }),
+    createFileSystemWatcher: jest.fn().mockImplementation(() => {
+        return {
+            onDidCreate: jest.fn(),
+            onDidChange: jest.fn(),
+            onDidDelete: jest.fn(),
+            disposable: new Disposable(),
+        };
+    }),
+    onDidOpenTextDocument: jest.fn().mockReturnValue(new Disposable()),
+    onDidChangeTextDocument: jest.fn().mockReturnValue(new Disposable()),
+    fs: {
+        readFile: jest.fn().mockImplementation((uri: Uri) => {
+            return new Promise((resolve) => resolve(readFileSync(uri.fsPath)));
+        }),
+    },
+};
+
+const RelativePattern = jest
+    .fn()
+    .mockImplementation((workspaceFolder: WorkspaceFolder, pattern: string) => {
+        return {
+            uri: workspaceFolder.uri,
+            base: workspaceFolder.uri.fsPath,
+            pattern,
+        };
+    });
+
+export {
+    workspace,
+    Disposable,
+    Range,
+    Position,
+    Location,
+    tests,
+    TestRunProfileKind,
+    TestMessage,
+    CancellationTokenSource,
+    RelativePattern,
+};
---
+++ src/__tests__/extension.test.ts
@@ -0,0 +1,121 @@
+import { describe, expect, it } from '@jest/globals';
+import { activate } from '../extension';
+import * as vscode from 'vscode';
+import { TestController, TextDocument, WorkspaceFolder } from 'vscode';
+import { glob, IOptions } from 'glob';
+import { readFileSync } from 'fs';
+import { URI } from 'vscode-uri';
+import * as path from 'path';
+
+const setTextDocuments = (textDocuments: TextDocument[]) => {
+    Object.defineProperty(vscode.workspace, 'textDocuments', {
+        value: textDocuments,
+    });
+};
+
+const setWorkspaceFolders = (workspaceFolders: WorkspaceFolder[]) => {
+    Object.defineProperty(vscode.workspace, 'workspaceFolders', {
+        value: workspaceFolders,
+    });
+};
+
+const globTextDocuments = (pattern: string, options?: IOptions) => {
+    options = { absolute: true, ignore: ['**/node_modules/**', '**/.git/**'], ...options };
+
+    return glob
+        .sync(pattern, options)
+        .map((file) => URI.file(file))
+        .map((uri) => ({
+            uri,
+            fileName: uri.fsPath,
+            getText: () => readFileSync(uri.fsPath).toString(),
+        })) as TextDocument[];
+};
+
+const _setTimeout = global.setTimeout;
+
+const useFakeTimers = (ms: number, fn: Function) => {
+    (global as any).setTimeout = (fn: any, _ms?: number) => fn();
+
+    return new Promise((resolve) => {
+        fn();
+        _setTimeout(() => resolve(true), ms);
+    });
+};
+
+const getTestController = () => {
+    return (vscode.tests.createTestController as jest.Mock).mock.results[0].value;
+};
+
+const getRunProfile = (ctrl: TestController) => {
+    return (ctrl.createRunProfile as jest.Mock).mock.results[0].value;
+};
+
+const getTestFile = (ctrl: TestController, pattern: RegExp) => {
+    const doc = vscode.workspace.textDocuments.find((doc) => doc.uri.fsPath.match(pattern))!;
+
+    return ctrl.items.get(doc.uri.toString());
+};
+
+const getTestRun = (ctrl: TestController) => {
+    return (ctrl.createTestRun as jest.Mock).mock.results[0].value;
+};
+
+describe('Extension Test', () => {
+    beforeEach(() => jest.clearAllMocks());
+
+    describe('activate()', () => {
+        const root = path.join(__dirname, '../../sample');
+        const context: any = { subscriptions: { push: jest.fn() } };
+
+        beforeEach(() => {
+            setWorkspaceFolders([{ index: 0, name: 'sample', uri: URI.file(root) }]);
+            setTextDocuments(globTextDocuments('**/*.md', { cwd: root }));
+            context.subscriptions.push.mockReset();
+        });
+
+        it('should activate extension', async () => {
+            await activate(context);
+            expect(context.subscriptions.push).toHaveBeenCalledTimes(2);
+        });
+
+        it('should run test', async () => {
+            await activate(context);
+
+            const ctrl = getTestController();
+            const runProfile = getRunProfile(ctrl);
+
+            const request = {
+                include: [getTestFile(ctrl, /test\.md$/)],
+                exclude: [],
+                profile: runProfile,
+            };
+
+            await useFakeTimers(500, () => {
+                runProfile.runHandler(request, new vscode.CancellationTokenSource().token);
+            });
+
+            const { enqueued, started, passed, failed, end } = getTestRun(ctrl);
+
+            expect(enqueued).toHaveBeenCalledTimes(4);
+            expect(started).toHaveBeenCalledTimes(4);
+            expect(passed).toHaveBeenCalledTimes(3);
+            expect(failed).toHaveBeenCalledTimes(1);
+            expect(end).toHaveBeenCalledTimes(1);
+        });
+
+        it('should refresh test', async () => {
+            await activate(context);
+            const ctrl = getTestController();
+
+            await ctrl.refreshHandler();
+        });
+
+        it('should resolve test', async () => {
+            await activate(context);
+            const ctrl = getTestController();
+
+            await ctrl.resolveHandler();
+        });
+    });
+});
