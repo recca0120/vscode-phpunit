ae89c77018e15ab599db6d2cc542ea6c16b3c81a
recca0120 <recca0120@gmail.com>
2022-12-08 01:34:59 +0800

    more path test

--- src/phpunit/command.test.ts
+++ src/phpunit/command.test.ts
@@ -6,11 +6,12 @@ import { projectPath } from './__tests__/helper';
 describe('Command Test', () => {
     describe('LocalCommand', () => {
         it('should add -f when phpunit binary is paratest and has --filter', () => {
+            const cwd = projectPath('');
             const configuration = new Configuration({
                 php: 'php',
                 phpunit: 'vendor/bin/paratest',
             });
-            const command = new LocalCommand(configuration);
+            const command = new LocalCommand(configuration, { cwd });
             command.setArguments("--filter='^.*::(test_passed)( with data set .*)?$'");
 
             const { cmd, args } = command.apply();
@@ -21,6 +22,31 @@ describe('Command Test', () => {
                 '--teamcity',
                 '--colors=never',
                 '-f',
+            ]);
+        });
+
+        it('should run Windows Path', () => {
+            const projectPath = (path: string) =>
+                `C:\\vscode\\${path}`.replace(/\//g, '\\').replace(/\\$/g, '');
+
+            const cwd = projectPath('');
+            const configuration = new Configuration({
+                php: 'php',
+                phpunit: `${projectPath('vendor/bin/phpunit')}`,
+            });
+            const command = new LocalCommand(configuration, { cwd: cwd });
+
+            const testFile = projectPath('tests/AssertionsTest.php');
+            command.setArguments(`${testFile} --filter='^.*::(test_passed)( with data set .*)?$'`);
+
+            const { cmd, args } = command.apply();
+            expect(cmd).toEqual('php');
+            expect(args).toEqual([
+                `${projectPath('vendor/bin/phpunit')}`,
+                `${projectPath('tests/AssertionsTest.php')}`,
+                '--filter=^.*::(test_passed)( with data set .*)?$',
+                '--teamcity',
+                '--colors=never',
             ]);
         });
     });
@@ -51,6 +77,7 @@ describe('Command Test', () => {
                 "php vendor/bin/paratest '--filter=^.*::(test_passed)( with data set .*)?$' '--teamcity' '--colors=never' '-f'",
             ]);
         });
+
         it('should replace workspaceFolder', () => {
             const cwd = projectPath('');
             const configuration = new Configuration({
@@ -63,11 +90,38 @@ describe('Command Test', () => {
             });
             const command = new RemoteCommand(configuration, { cwd: cwd });
 
-            command.setArguments(
-                `${projectPath(
-                    'tests/AssertionsTest.php'
-                )} --filter='^.*::(test_passed)( with data set .*)?$'`
-            );
+            const testFile = projectPath('tests/AssertionsTest.php');
+            command.setArguments(`${testFile} --filter='^.*::(test_passed)( with data set .*)?$'`);
+
+            const { cmd, args } = command.apply();
+            expect(cmd).toEqual('docker');
+            expect(args).toEqual([
+                'exec',
+                '--workdir=/var/www/',
+                'container_name',
+                'bash',
+                '-c',
+                "php vendor/bin/phpunit /var/www/tests/AssertionsTest.php '--filter=^.*::(test_passed)( with data set .*)?$' '--teamcity' '--colors=never'",
+            ]);
+        });
+
+        it('should replace workspaceFolder for Windows Path', () => {
+            const projectPath = (path: string) =>
+                `C:\\vscode\\${path}`.replace(/\//g, '\\').replace(/\\$/g, '');
+
+            const cwd = projectPath('');
+            const configuration = new Configuration({
+                command: 'docker exec --workdir=/var/www/ container_name bash -c',
+                php: 'php',
+                phpunit: 'vendor/bin/phpunit',
+                paths: {
+                    '${workspaceFolder}': '/var/www',
+                },
+            });
+            const command = new RemoteCommand(configuration, { cwd: cwd });
+
+            const testFile = projectPath('tests/AssertionsTest.php');
+            command.setArguments(`${testFile} --filter='^.*::(test_passed)( with data set .*)?$'`);
 
             const { cmd, args } = command.apply();
             expect(cmd).toEqual('docker');
