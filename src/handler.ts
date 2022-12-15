2c92769056f25e83d41eae6f80dd689d97f8cb46
recca0120 <recca0120@gmail.com>
2022-12-16 05:14:13 +0800

    refactor Handler

--- src/handler.ts
+++ src/handler.ts
@@ -4,6 +4,7 @@ import {
     TestController,
     TestItem,
     TestItemCollection,
+    TestRun,
     TestRunRequest,
     window,
     workspace,
@@ -31,23 +32,44 @@ export class Handler {
     async run(request: TestRunRequest, cancellation: CancellationToken) {
         this.latestTestRunRequest = request;
 
-        const queue: { test: TestItem }[] = [];
         const run = this.ctrl.createTestRun(request);
+        const queue: { test: TestItem }[] = [];
 
-        const discoverTests = async (tests: Iterable<TestItem>) => {
-            for (const test of tests) {
-                if (request.exclude?.includes(test)) {
-                    continue;
-                }
+        return this.discoverTests(
+            request.include ?? this.gatherTestItems(this.ctrl.items),
+            request,
+            run,
+            queue
+        ).then(async () => await this.runTestQueue(request, run, queue, cancellation));
+    }
 
-                if (!test.canResolveChildren) {
-                    run.enqueued(test);
-                    queue.push({ test });
-                } else {
-                    await discoverTests(this.gatherTestItems(test.children));
-                }
-            }
-        };
+    private async runTestQueue(
+        request: TestRunRequest,
+        run: TestRun,
+        queue: { test: TestItem }[],
+        cancellation: CancellationToken
+    ) {
+        const currentWorkspaceFolder = await this.getCurrentWorkspaceFolder();
+        if (!currentWorkspaceFolder) {
+            run.end();
+            return;
+        }
+
+        const options = { cwd: currentWorkspaceFolder.uri.fsPath };
+
+        const command = ((this.configuration.get('command') as string) ?? '').match(/docker|ssh/)
+            ? new RemoteCommand(this.configuration, options)
+            : new LocalCommand(this.configuration, options);
+
+        const runner = new TestRunner();
+        runner.observe(new TestResultObserver(queue, run, cancellation));
+        runner.observe(new OutputChannelObserver(this.outputChannel, this.configuration));
+
+        if (!request.include) {
+            await runner.run(command);
+
+            return;
+        }
 
         const getArguments = (test: TestItem) => {
             return !test.parent
@@ -55,38 +77,29 @@ export class Handler {
                 : this.testData.get(test.parent.uri!.toString())!.getArguments(test.id);
         };
 
-        const runTestQueue = async () => {
-            const currentWorkspaceFolder = await this.getCurrentWorkspaceFolder();
-            if (!currentWorkspaceFolder) {
-                run.end();
-                return;
-            }
-            const options = { cwd: currentWorkspaceFolder.uri.fsPath };
-
-            const command = ((this.configuration.get('command') as string) ?? '').match(
-                /docker|ssh/
-            )
-                ? new RemoteCommand(this.configuration, options)
-                : new LocalCommand(this.configuration, options);
-
-            const runner = new TestRunner();
-            runner.observe(new TestResultObserver(queue, run, cancellation));
-            runner.observe(new OutputChannelObserver(this.outputChannel, this.configuration));
-
-            if (!request.include) {
-                await runner.run(command);
-
-                return;
-            }
-
-            await Promise.all(
-                request.include.map((test) => runner.run(command.setArguments(getArguments(test))))
-            );
-        };
-
-        return discoverTests(request.include ?? this.gatherTestItems(this.ctrl.items)).then(
-            runTestQueue
+        await Promise.all(
+            request.include.map((test) => runner.run(command.setArguments(getArguments(test))))
         );
+    }
+
+    private async discoverTests(
+        tests: Iterable<TestItem>,
+        request: TestRunRequest,
+        run: TestRun,
+        queue: { test: TestItem }[]
+    ) {
+        for (const test of tests) {
+            if (request.exclude?.includes(test)) {
+                continue;
+            }
+
+            if (!test.canResolveChildren) {
+                run.enqueued(test);
+                queue.push({ test });
+            } else {
+                await this.discoverTests(this.gatherTestItems(test.children), request, run, queue);
+            }
+        }
     }
 
     private async getCurrentWorkspaceFolder() {
