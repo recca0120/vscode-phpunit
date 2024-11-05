ab43efc42c893bbea1cc48030edf2f01623a533b
recca0120 <recca0120@gmail.com>
2024-11-06 04:25:00 +0800

    inline

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,10 @@
 import * as vscode from 'vscode';
+import { CancellationToken, CancellationTokenSource, commands, EventEmitter, OutputChannel, Position, Range, TestController, TestItem, TestItemCollection, TestRun, TestRunProfile, TestRunRequest, Uri, window, workspace } from 'vscode';
 import { Configuration } from './configuration';
-import { TestFile } from './testFile';
-import { Handler } from './handler';
-import { CommandHandler } from './commandHandler';
-import { PHPUnitXML } from './phpunit';
+import { Command, LocalCommand, PHPUnitXML, RemoteCommand, Test, TestCaseParser, TestRunner } from './phpunit';
 import { readFile, stat } from 'node:fs/promises';
 import * as path from 'node:path';
+import { OutputChannelObserver, TestResultObserver } from './observers';
 
 async function findAsyncSequential<T>(
     array: T[],
@@ -33,101 +32,26 @@ async function checkFileExists(filePath: string): Promise<boolean> {
     }
 }
 
-const testData = new Map<string, TestFile>();
 
-export async function activate(context: vscode.ExtensionContext) {
-    const configuration = new Configuration(vscode.workspace.getConfiguration('phpunit'));
-    context.subscriptions.push(
-        vscode.workspace.onDidChangeConfiguration(() =>
-            configuration.updateWorkspaceConfiguration(vscode.workspace.getConfiguration('phpunit')),
-        ),
-    );
-
-    const outputChannel = vscode.window.createOutputChannel('PHPUnit');
-    context.subscriptions.push(outputChannel);
-
-    const ctrl = vscode.tests.createTestController('phpUnitTestController', 'PHPUnit');
-    context.subscriptions.push(ctrl);
-
-    const fileChangedEmitter = new vscode.EventEmitter<vscode.Uri>();
-    const handler = new Handler(
-        testData,
-        configuration,
-        outputChannel,
-        ctrl,
-        fileChangedEmitter,
-        getOrCreateFile,
-    );
-
-    ctrl.refreshHandler = async () => {
-        await Promise.all(
-            (await getWorkspaceTestPatterns()).map(({ pattern, exclude }) =>
-                findInitialFiles(ctrl, pattern, exclude),
-            ),
-        );
-    };
-
-    const testRunProfile = ctrl.createRunProfile(
-        'Run Tests',
-        vscode.TestRunProfileKind.Run,
-        (request, cancellation) => handler.run(request, cancellation),
-        true,
-        undefined,
-        true,
-    );
-
-    ctrl.resolveHandler = async (item) => {
-        if (!item) {
-            context.subscriptions.push(...(await startWatchingWorkspace(ctrl, fileChangedEmitter)));
-        }
-    };
-
-    async function updateNodeForDocument(e: vscode.TextDocument) {
-        if (e.uri.scheme !== 'file' || !e.uri.path.endsWith('.php')) {
-            return;
-        }
-
-        const currentWorkspaceFolder = vscode.workspace.getWorkspaceFolder(e.uri);
-        const workspaceTestPattern = (await getWorkspaceTestPatterns()).find(({ workspaceFolder }) => {
-            return currentWorkspaceFolder!.name === workspaceFolder.name;
-        });
-
-        if (!workspaceTestPattern) {
-            return;
-        }
-
-        if (vscode.languages.match({ pattern: workspaceTestPattern.exclude.pattern }, e) !== 0) {
-            return;
-        }
-
-        await getOrCreateFile(ctrl, e.uri);
+async function updateNodeForDocument(e: vscode.TextDocument, ctrl: vscode.TestController) {
+    if (e.uri.scheme !== 'file' || !e.uri.path.endsWith('.php')) {
+        return;
     }
 
-    testData.clear();
-    await Promise.all(
-        vscode.workspace.textDocuments.map((document) => updateNodeForDocument(document)),
-    );
+    const currentWorkspaceFolder = vscode.workspace.getWorkspaceFolder(e.uri);
+    const workspaceTestPattern = (await getWorkspaceTestPatterns()).find(({ workspaceFolder }) => {
+        return currentWorkspaceFolder!.name === workspaceFolder.name;
+    });
 
-    context.subscriptions.push(
-        vscode.workspace.onDidOpenTextDocument(updateNodeForDocument),
-        vscode.workspace.onDidChangeTextDocument((e) => updateNodeForDocument(e.document)),
-    );
+    if (!workspaceTestPattern) {
+        return;
+    }
 
-    context.subscriptions.push(
-        vscode.commands.registerCommand('phpunit.reload', async () => {
-            await Promise.all(
-                (await getWorkspaceTestPatterns()).map(({ pattern, exclude }) =>
-                    findInitialFiles(ctrl, pattern, exclude),
-                ),
-            );
-        }),
-    );
+    if (vscode.languages.match({ pattern: workspaceTestPattern.exclude.pattern }, e) !== 0) {
+        return;
+    }
 
-    const commandHandler = new CommandHandler(testRunProfile, testData);
-    context.subscriptions.push(commandHandler.runAll());
-    context.subscriptions.push(commandHandler.runFile());
-    context.subscriptions.push(commandHandler.runTestAtCursor());
-    context.subscriptions.push(commandHandler.rerun(handler));
+    await getOrCreateFile(ctrl, e.uri);
 }
 
 export async function getOrCreateFile(controller: vscode.TestController, uri: vscode.Uri) {
@@ -237,3 +161,393 @@ async function startWatchingWorkspace(controller: vscode.TestController, fileCha
         return watcher;
     });
 }
+
+export class Handler {
+    private latestTestRunRequest: TestRunRequest | undefined;
+
+    constructor(
+        private testData: Map<string, TestFile>,
+        private configuration: Configuration,
+        private outputChannel: OutputChannel,
+        private ctrl: TestController,
+        private fileChangedEmitter: EventEmitter<Uri>,
+        private getOrCreateFile: Function,
+    ) {
+    }
+
+    getLatestTestRunRequest() {
+        return this.latestTestRunRequest;
+    }
+
+    async run(request: TestRunRequest, cancellation: CancellationToken) {
+        if (!request.continuous) {
+            return this.startTestRun(request, cancellation);
+        }
+
+        const l = this.fileChangedEmitter.event(async (uri) => {
+            await this.getOrCreateFile(this.ctrl, uri);
+
+            await this.startTestRun(
+                new TestRunRequest(
+                    request.include ?? [],
+                    undefined,
+                    request.profile,
+                    true,
+                ),
+                cancellation,
+            );
+        });
+        cancellation.onCancellationRequested(() => l.dispose());
+    }
+
+    private async startTestRun(request: TestRunRequest, cancellation: CancellationToken) {
+        const command = await this.createCommand();
+
+        if (!command) {
+            return;
+        }
+
+        this.latestTestRunRequest = request;
+
+        const run = this.ctrl.createTestRun(request);
+        const queueHandler = new TestQueueHandler(request, run, this.testData);
+        const runner = this.createTestRunner(queueHandler, run, request, cancellation);
+
+        await queueHandler.discoverTests(request.include ?? gatherTestItems(this.ctrl.items));
+        await queueHandler.runQueue(runner, command);
+    }
+
+    private async createCommand() {
+        const workspaceFolder = await getCurrentWorkspaceFolder();
+        if (!workspaceFolder) {
+            return;
+        }
+
+        const options = { cwd: workspaceFolder!.uri.fsPath };
+
+        return this.isRemote()
+            ? new RemoteCommand(this.configuration, options)
+            : new LocalCommand(this.configuration, options);
+    }
+
+    private isRemote() {
+        const command = (this.configuration.get('command') as string) ?? '';
+
+        return command.match(/docker|ssh|sail/) !== null;
+    }
+
+    private createTestRunner(
+        queueHandler: TestQueueHandler,
+        run: TestRun,
+        request: TestRunRequest,
+        cancellation: CancellationToken,
+    ) {
+        const runner = new TestRunner();
+        runner.observe(new TestResultObserver(queueHandler.queue, run, cancellation));
+        runner.observe(new OutputChannelObserver(this.outputChannel, this.configuration, request));
+
+        return runner;
+    }
+}
+
+class TestQueueHandler {
+    public queue: { test: TestItem }[] = [];
+
+    constructor(
+        private request: TestRunRequest,
+        private run: TestRun,
+        private testData: Map<string, TestFile>,
+    ) {
+    }
+
+    public async discoverTests(tests: Iterable<TestItem>) {
+        for (const test of tests) {
+            if (this.request.exclude?.includes(test)) {
+                continue;
+            }
+
+            if (!test.canResolveChildren) {
+                this.run.enqueued(test);
+                this.queue.push({ test });
+            } else {
+                await this.discoverTests(gatherTestItems(test.children));
+            }
+        }
+    }
+
+    public async runQueue(runner: TestRunner, command: Command) {
+        if (!this.request.include) {
+            return runner.run(command);
+        }
+
+        return await Promise.all(
+            this.request.include.map((test) =>
+                runner.run(command.setArguments(this.getTestArguments(test))),
+            ),
+        );
+    }
+
+    private getTestArguments(test: TestItem) {
+        return !test.parent
+            ? test.uri!.fsPath
+            : this.testData.get(test.parent.uri!.toString())!.getArguments(test.id);
+    }
+}
+
+function gatherTestItems(collection: TestItemCollection) {
+    const items: TestItem[] = [];
+    collection.forEach((item) => items.push(item));
+    return items;
+}
+
+async function getCurrentWorkspaceFolder() {
+    if (!workspace.workspaceFolders || workspace.workspaceFolders.length === 0) {
+        return null;
+    }
+
+    if (workspace.workspaceFolders.length === 1) {
+        return workspace.workspaceFolders[0];
+    }
+
+    if (window.activeTextEditor) {
+        return workspace.getWorkspaceFolder(window.activeTextEditor.document.uri);
+    }
+
+    return window.showWorkspaceFolderPick();
+}
+
+const textDecoder = new TextDecoder('utf-8');
+
+const parser = new TestCaseParser();
+
+export class TestFile {
+    private suites: Test[] = [];
+    private testItems: TestItem[] = [];
+
+    constructor(public uri: Uri) {
+    }
+
+    async update(ctrl: TestController) {
+        const rawContent = textDecoder.decode(await workspace.fs.readFile(this.uri));
+        parser.parse(rawContent, this.uri.fsPath, {
+            onSuite: (suite: Test) => {
+                const testItem = ctrl.createTestItem(suite.id, suite.label, this.uri);
+                testItem.canResolveChildren = true;
+                testItem.sortText = suite.id;
+                testItem.range = new Range(
+                    new Position(suite.start.line - 1, suite.start.character),
+                    new Position(suite.end.line - 1, suite.end.character),
+                );
+
+                ctrl.items.add(testItem);
+                this.suites.push(suite);
+            },
+            onTest: (test: Test, index) => {
+                const testItem = ctrl.createTestItem(test.id, test.label, this.uri);
+                testItem.canResolveChildren = false;
+                testItem.sortText = `${index}`;
+                testItem.range = new Range(
+                    new Position(test.start.line - 1, test.start.character),
+                    new Position(test.end.line - 1, test.end.character),
+                );
+
+                ctrl.items.get(test.parent!.id)!.children.add(testItem);
+                this.testItems.push(testItem);
+            },
+        });
+
+        return this;
+    }
+
+    delete(ctrl: TestController) {
+        this.testItems.forEach((testItem) => ctrl.items.delete(testItem.id));
+        this.suites = [];
+        this.testItems = [];
+    }
+
+    getArguments(testId: string): string {
+        const test = this.findTest(testId);
+
+        return test ? `${(this.asFilter(test) ?? '')} ${encodeURIComponent(test.file)}` : '';
+    }
+
+    getTestItems() {
+        return this.testItems;
+    }
+
+    findTestItemByPosition(position: Position) {
+        return (
+            this.doFindTestItem(this.testItems, (testItem: TestItem) => {
+                if (testItem.canResolveChildren) {
+                    return false;
+                }
+
+                const range = testItem.range!;
+
+                return position.line >= range.start.line && position.line <= range.end.line;
+            }) ?? this.testItems[0]
+        );
+    }
+
+    private doFindTestItem(testItems: TestItem[], filter: (testItem: TestItem) => boolean): TestItem | void {
+        for (const testItem of testItems) {
+            if (filter(testItem)) {
+                return testItem;
+            }
+
+            if (testItem.children.size > 0) {
+                return this.doFindTestItem(this.gatherTestItems(testItem.children), filter);
+            }
+        }
+    }
+
+    private gatherTestItems(collection: TestItemCollection) {
+        const items: TestItem[] = [];
+        collection.forEach((item) => items.push(item));
+
+        return items;
+    }
+
+    private findTest(testId: string) {
+        return this.doFindTest(this.suites, (test: Test) => testId === test.id);
+    }
+
+    private doFindTest(tests: Test[], filter: (test: Test) => boolean): Test | void {
+        for (const test of tests) {
+            if (filter(test)) {
+                return test;
+            }
+
+            if (test.children.length > 0) {
+                return this.doFindTest(test.children, filter);
+            }
+        }
+    }
+
+    private asFilter(test: Test) {
+        const deps = [test.method, ...(test.annotations.depends ?? [])].join('|');
+
+        return test.children.length > 0 ? '' : `--filter '^.*::(${deps})( with data set .*)?$'`;
+    }
+}
+
+export class CommandHandler {
+    constructor(private testRunProfile: TestRunProfile, private testData: Map<string, TestFile>) {
+    }
+
+    runAll() {
+        return commands.registerCommand('phpunit.run-all', () => {
+            this.run(undefined);
+        });
+    }
+
+    runFile() {
+        return commands.registerCommand('phpunit.run-file', () => {
+            const testFile = this.findTestFile();
+
+            if (testFile) {
+                this.run(testFile.getTestItems());
+            }
+        });
+    }
+
+    runTestAtCursor() {
+        return commands.registerCommand('phpunit.run-test-at-cursor', () => {
+            const testFile = this.findTestFile();
+
+            if (testFile) {
+                this.run([
+                    testFile.findTestItemByPosition(window.activeTextEditor!.selection.active)!,
+                ]);
+            }
+        });
+    }
+
+    rerun(handler: Handler) {
+        return commands.registerCommand('phpunit.rerun', () => {
+            const latestTestRunRequest = handler.getLatestTestRunRequest();
+
+            return latestTestRunRequest
+                ? this.runRequest(latestTestRunRequest)
+                : this.run(undefined);
+        });
+    }
+
+    private run(include: readonly TestItem[] | undefined) {
+        this.runRequest(new TestRunRequest(include, undefined, this.testRunProfile));
+    }
+
+    private runRequest(request: TestRunRequest) {
+        const cancellation = new CancellationTokenSource().token;
+
+        this.testRunProfile.runHandler(request, cancellation);
+    }
+
+    private findTestFile(): TestFile | null {
+        if (!window.activeTextEditor) {
+            return null;
+        }
+
+        return this.testData.get(window.activeTextEditor.document.uri.toString())!;
+    }
+}
+
+const testData = new Map<string, TestFile>();
+
+export async function activate(context: vscode.ExtensionContext) {
+    const configuration = new Configuration(vscode.workspace.getConfiguration('phpunit'));
+    context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(() => configuration.updateWorkspaceConfiguration(vscode.workspace.getConfiguration('phpunit'))));
+
+    const outputChannel = vscode.window.createOutputChannel('PHPUnit');
+    context.subscriptions.push(outputChannel);
+
+    const ctrl = vscode.tests.createTestController('phpUnitTestController', 'PHPUnit');
+    context.subscriptions.push(ctrl);
+
+    testData.clear();
+    await Promise.all(
+        vscode.workspace.textDocuments.map((document) => {
+            return updateNodeForDocument(document, ctrl);
+        }),
+    );
+
+    const reload = async () => {
+        await Promise.all(
+            (await getWorkspaceTestPatterns()).map(({ pattern, exclude }) =>
+                findInitialFiles(ctrl, pattern, exclude),
+            ),
+        );
+    };
+
+    context.subscriptions.push(
+        vscode.workspace.onDidOpenTextDocument((document) => updateNodeForDocument(document, ctrl)),
+        vscode.workspace.onDidChangeTextDocument((e) => updateNodeForDocument(e.document, ctrl)),
+    );
+
+    ctrl.refreshHandler = reload;
+    ctrl.resolveHandler = async (item) => {
+        if (!item) {
+            context.subscriptions.push(...(await startWatchingWorkspace(ctrl, fileChangedEmitter)));
+        }
+    };
+
+    const fileChangedEmitter = new vscode.EventEmitter<vscode.Uri>();
+    const handler = new Handler(testData, configuration, outputChannel, ctrl, fileChangedEmitter, getOrCreateFile);
+
+    const testRunProfile = ctrl.createRunProfile(
+        'Run Tests',
+        vscode.TestRunProfileKind.Run,
+        (request, cancellation) => handler.run(request, cancellation),
+        true,
+        undefined,
+        true,
+    );
+
+    const commandHandler = new CommandHandler(testRunProfile, testData);
+    context.subscriptions.push(vscode.commands.registerCommand('phpunit.reload', reload));
+    context.subscriptions.push(commandHandler.runAll());
+    context.subscriptions.push(commandHandler.runFile());
+    context.subscriptions.push(commandHandler.runTestAtCursor());
+    context.subscriptions.push(commandHandler.rerun(handler));
+}
+
