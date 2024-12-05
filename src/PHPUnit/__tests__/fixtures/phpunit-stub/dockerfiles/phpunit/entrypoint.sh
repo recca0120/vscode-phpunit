a949693c74457eee75a71030aeb16a27b93c564c
recca0120 <recca0120@gmail.com>
2024-12-05 15:46:39 +0800

    update

--- src/PHPUnit/__tests__/fixtures/phpunit-stub/dockerfiles/phpunit/entrypoint.sh
+++ src/PHPUnit/__tests__/fixtures/phpunit-stub/dockerfiles/phpunit/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# generate host keys if not present
 ssh-keygen -A
+chmod 600 /root/.ssh/authorized_keys
 
 # do not detach (-D), log to stderr (-e), passthrough other arguments
 exec /usr/sbin/sshd -D -e "$@"
