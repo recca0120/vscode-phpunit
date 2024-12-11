8c7091ba3d7dedc607e938d0682351ad52dad8ec
recca0120 <recca0120@gmail.com>
2024-12-11 23:20:36 +0800

    fixture from pest

---
+++ src/PHPUnit/__tests__/fixtures/pest-stub/tests/Unit/Arch/ToHaveMethod/HasMethod/HasMethod.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Fixtures\Arch\ToHaveMethod\HasMethod;
+
+class HasMethod
+{
+    public function foo(): void {}
+}
---
+++ src/PHPUnit/__tests__/fixtures/pest-stub/tests/Unit/Arch/ToHaveMethod/HasMethod/ParentHasMethodClass.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Fixtures\Arch\ToHaveMethod\HasMethod;
+
+class ParentHasMethodClass
+{
+    public function foo(): void {}
+}
