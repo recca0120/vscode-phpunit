1e71dd66b443f7403e74c7f60b3ef795d103b889
Tim <tinghom@gmail.com>
2025-01-04 16:45:28 +0800

    check if parser is Pest when locationHint starts with 'file://'

--- src/PHPUnit/TestParser/Transformers/TransformerFactory.ts
+++ src/PHPUnit/TestParser/Transformers/TransformerFactory.ts
@@ -1,13 +1,56 @@
 import { PestTransformer } from './PestTransformer';
 import { PHPUnitTransformer } from './PHPUnitTransformer';
+import { AppState } from '../../../AppState';
+
+interface TransformerInput {
+    classFQN?: string;
+    locationHint?: string;
+    testResultName?: string;
+}
 
 export abstract class TransformerFactory {
-    static isPest(text: string) {
-        return /^pest|^P\\|pest_qn:\/\//i.test(text);
+    static getTextFromInput(input: TransformerInput | string): string {
+        if (typeof input === 'string') {
+            return input;
+        }
+
+        return input.classFQN || input.locationHint || '';
     }
 
-    static factory(text: string) {
-        return this.isPest(text) ? new PestTransformer() : new PHPUnitTransformer();
+    static isPest(input: TransformerInput | string) {
+        const text = this.getTextFromInput(input);
+
+        if (/^pest|^P\\|pest_qn:\/\//i.test(text)) {
+            return true;
+        }
+
+        if (/php_qn:\/\//i.test(text)) {
+            return false;
+        }
+
+        if (text.startsWith('file://')) {
+            return this.checkByParserId(input);
+        }
+
+        return false;
+    }
+
+    static checkByParserId(input: TransformerInput | string): boolean {
+        if (typeof input === 'string' || typeof input.locationHint === 'undefined') {
+            return false;
+        }
+
+        const parserId = input.locationHint
+            .split('://')[1]
+            .replace(/^tests\//i, 'Tests\\')
+            .replace(/\//g, '\\')
+            .replace(/\.php$/, '');
+
+        return AppState.getParserTestType(parserId) ?? false;
+    }
+
+    static factory(input: TransformerInput | string) {
+        return this.isPest(input) ? new PestTransformer() : new PHPUnitTransformer();
     }
 }
 
