7fd0fb31963bf50a909f6dcfb77c17f1662c4e71
recca0120 <recca0120@gmail.com>
2025-03-05 00:34:36 +0800

    bump

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,32 +1,32 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 
 export default [{
-    files: ["**/*.ts"],
+    files: ['**/*.ts'],
     ignores: [
-        "**/out",
-        "**/dist",
-        "**/*.d.ts",
-        "eslint.config.mjs",
+        '**/out',
+        '**/dist',
+        '**/*.d.ts',
+        'eslint.config.mjs',
     ],
 }, {
     plugins: {
-        "@typescript-eslint": typescriptEslint,
+        '@typescript-eslint': typescriptEslint,
     },
 
     languageOptions: {
         parser: tsParser,
         ecmaVersion: 6,
-        sourceType: "module",
+        sourceType: 'module',
     },
 
     rules: {
-        'semi': [2, "always"],
-        'curly': ["warn"],
-        'eqeqeq': ["warn"],
+        'semi': [2, 'always'],
+        'curly': ['warn'],
+        'eqeqeq': ['warn'],
         '@typescript-eslint/no-explicit-any': 0,
         '@typescript-eslint/explicit-module-boundary-types': 0,
         '@typescript-eslint/no-non-null-assertion': 0,
-        "@typescript-eslint/naming-convention": ["warn"],
+        '@typescript-eslint/naming-convention': ['warn'],
     },
 }];
